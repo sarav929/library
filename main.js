af5ecbd962691422afaac2397130696f6709e384
Sara <123300249+sarav929@users.noreply.github.com>
2024-09-20 11:47:57 +0100

    add functions to remove book & update read status add close button to modal

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const myLibrary = []
 let newBook
 
 const addBook = document.getElementById('add-book')
+const closeModal = document.getElementById('close-modal')
 const modal = document.getElementById('modal')
 const form = document.getElementById('form')
 const library = document.getElementById('library')
@@ -17,16 +18,29 @@ function addBookToLibrary(array, book) {
   array.push(book)
 }
 
+function printLibrary(array) {
+    library.innerHTML = ""
+    for (i = 0; i < array.length; i++) {
+        addBookCard(array[i])
+    }
+    setButtons()
+}
+
 function addBookCard(book) {
     const card = document.createElement("div")
     library.appendChild(card)
+    let buttonText 
+
+    if (book.read == "✔ Read") {
+        buttonText = "Mark as Unread"
+    } else {
+        buttonText = "Mark as Read"
+    }
        
-    
     card.innerHTML = `<div class="status">${book.read}</div><p>${book.title}</p> <p>${book.author}</p> <p>${book.pages} pages </p>
     <button class="remove-book" id="${myLibrary.indexOf(book)}">remove</button>
-    <button class="mark-read" id="${myLibrary.indexOf(book)}">mark as unread</button>`
-    card.classList.add("book-card")
-    
+    <button class="mark-read" id="${myLibrary.indexOf(book)}">${buttonText}</button>`
+    card.classList.add("book-card")   
 }
 
 function setButtons() {
@@ -36,27 +50,32 @@ function setButtons() {
 
     markReadArray.forEach((btn) => {
             btn.addEventListener("click", function() {
-                console.log(myLibrary[btn.id].read)
+                if (myLibrary[btn.id].read == "✔ Read") {
+                    myLibrary[btn.id].read = "✖ Not Read"
+                    printLibrary(myLibrary)
+                } else {
+                    myLibrary[btn.id].read = "✔ Read"
+                    printLibrary(myLibrary)
+                }
             })
         })
 
     removeBookArray.forEach((btn) => {
         btn.addEventListener("click", function() {
-            console.log(myLibrary[btn.id])
+            myLibrary.splice(btn.id, 1)
+            printLibrary(myLibrary)
         })
     })
-}
-
-// Print out library //
-
-for (i = 0; i < myLibrary.length; i++) {
-    addBookCard(myLibrary[i])
 }
 
 // show modal with form when newBook is clicked //
 
 addBook.addEventListener('click', () => {
     modal.show()
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close()
 })
 
 // create new book and add it to library
@@ -78,9 +97,7 @@ form.addEventListener('submit', () => {
     newBook = new Book(title.value, author.value, pages.value, status)
 
     addBookToLibrary(myLibrary, newBook)
-    addBookCard(newBook)
-    setButtons()
-
+    printLibrary(myLibrary)
 })
 
 
